dfcaa0c0f3be958ef6c674f871d6f80a9794823c
Dark <158550285+DarkPlus2@users.noreply.github.com>
2025-10-15 09:26:04 +0600

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,26 +1,10 @@
-// lib/db.ts
-import pkg from "pg";
-const { Pool } = pkg;
+import { Pool } from "pg";
 
-declare global {
-  // eslint-disable-next-line no-var
-  var __pgPool: any;
-}
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // Vercel SSL
+});
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("Please set DATABASE_URL in env");
-}
+export const query = (text: string, params?: any[]) => pool.query(text, params);
 
-if (!global.__pgPool) {
-  global.__pgPool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
-  });
-}
-
-export const pool = global.__pgPool as pkg.Pool;
-
-export async function query(text: string, params?: any[]) {
-  const res = await pool.query(text, params || []);
-  return res;
-}
+export default pool;
