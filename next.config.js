0d71accfd2479e3e2e4af2cee6d5dabce1d0e7dc
Dark <158550285+DarkPlus2@users.noreply.github.com>
2025-10-15 08:39:06 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { domains: ["cdn.myanimelist.net"] } // add your poster domains
-}
+  images: { domains: ["cdn.myanimelist.net", "image.tmdb.org"] } 
+};
 module.exports = nextConfig;
