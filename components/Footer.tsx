85a34fe9392da98f16c3f2fa21b6d72d10acd40e
Dark <158550285+DarkPlus2@users.noreply.github.com>
2025-10-16 14:11:58 +0600

    Delete components directory

--- components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-// components/Footer.tsx
-export default function Footer() {
-  return (
-    <footer className="bg-primary text-white p-6 text-center mt-6 shadow-inner">
-      <p>© {new Date().getFullYear()} ZeAnime. All rights reserved.</p>
-    </footer>
-  );
-}
