55bb25501f32fb66f423f54e374bec19287bf98f
Dark <158550285+DarkPlus2@users.noreply.github.com>
2025-10-16 17:24:19 +0600

    Update PlayerEmbed.tsx

--- src/components/PlayerEmbed.tsx
+++ src/components/PlayerEmbed.tsx
@@ -1,4 +1,8 @@
-export default function PlayerEmbed({ url }) {
+interface PlayerEmbedProps {
+  url: string;
+}
+
+export default function PlayerEmbed({ url }: PlayerEmbedProps) {
   return (
     <div className="aspect-video w-full">
       <iframe src={url} className="w-full h-full" allowFullScreen />
