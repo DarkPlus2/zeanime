46430103cfd31f67981126e63e3634338a14637c
Dark <158550285+DarkPlus2@users.noreply.github.com>
2025-10-16 16:18:52 +0600

    Delete src/components directory

--- src/components/ui/Loader.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loader() {
-  return (
-    <div className="flex items-center justify-center h-64">
-      <div className="w-12 h-12 border-4 border-zaccent border-t-transparent rounded-full animate-spin"></div>
-    </div>
-  );
-}
