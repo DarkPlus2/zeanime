2b04496593833f16fcba7d44ae61c5acf32dd18d
Dark <158550285+DarkPlus2@users.noreply.github.com>
2025-10-16 23:07:58 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
-import { getAllAnimes } from "@/lib/getAnime";
-import { AnimeGrid } from "@/components/AnimeGrid";
+import { prisma } from "@/lib/prisma";
+import AnimeList from "@/components/AnimeList";
+import { Anime } from "@/components/AnimeCard";
 
 export default async function HomePage() {
-  const animes = await getAllAnimes();
+  const animes: Anime[] = await prisma.anime.findMany({
+    orderBy: { id: "asc" },
+    take: 20,
+  });
 
   return (
-    <div>
-      <h1 className="text-4xl font-bold mb-6">Zeanime</h1>
-      <AnimeGrid animes={animes} />
+    <div className="space-y-8">
+      <h1 className="text-4xl font-bold">Zeanime</h1>
+      <AnimeList animes={animes} />
     </div>
   );
 }
