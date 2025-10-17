85a0cbb6b52f13689e76052af931ecf345ec6bfc
Dark <158550285+DarkPlus2@users.noreply.github.com>
2025-10-17 22:50:44 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ["anilist.co", "filemoon.sx", "abyss.to"],
-  },
-  // Remove experimental.appDir since Next.js 14 enables App Router by default
-};
+  experimental: {
+    appDir: true
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
