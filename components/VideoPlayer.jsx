036db06b6b2097fc188714f5cdc298db0c1071bb
Dark <158550285+DarkPlus2@users.noreply.github.com>
2025-10-17 23:21:37 +0600

    Update VideoPlayer.jsx

--- components/VideoPlayer.jsx
+++ components/VideoPlayer.jsx
@@ -1,18 +1,35 @@
-'use client'
+"use client";
+import React, { useEffect, useRef } from "react";
+import Plyr from "plyr";
+import "plyr/dist/plyr.css";
 
-import React from 'react'
-import { Plyr } from 'plyr-react'
-import 'plyr/dist/plyr.css'
+export default function VideoPlayer({ src, title }) {
+  const videoRef = useRef(null);
 
-export default function VideoPlayer({ src, poster }){
-  const source = {
-    type: 'video',
-    sources: [ { src, provider: 'html5' } ]
-  }
+  useEffect(() => {
+    if (videoRef.current) {
+      const player = new Plyr(videoRef.current, {
+        controls: [
+          "play-large",
+          "play",
+          "progress",
+          "current-time",
+          "mute",
+          "volume",
+          "settings",
+          "fullscreen",
+        ],
+      });
+      return () => player.destroy();
+    }
+  }, []);
 
   return (
-    <div className="card p-4">
-      <Plyr source={source} options={{ autoplay: false }} />
+    <div className="w-full max-w-5xl mx-auto mt-6">
+      <video ref={videoRef} className="w-full rounded-xl shadow-lg" playsInline controls>
+        <source src={src} type="video/mp4" />
+        {title && <track kind="captions" label={title} />}
+      </video>
     </div>
-  )
+  );
 }
