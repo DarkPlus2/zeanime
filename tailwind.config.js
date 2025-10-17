2c12fd7d6d8f5851d5d259f1b25c9eb89326149e
Dark <158550285+DarkPlus2@users.noreply.github.com>
2025-10-17 22:51:15 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: { extend: {} },
-  plugins: [],
+export default {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        accent: '#7c3aed'
+      }
+    }
+  },
+  plugins: []
 }
